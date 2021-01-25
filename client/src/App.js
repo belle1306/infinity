467f5fcfa092cf866c5e0f60e775ec3ffe71ee4f
Khalilah <dygkhalilah@gmail.com>
2021-01-25 17:38:35 +0800

    LogIn Form Added Test 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-
 import './App.css';
+import Form from './components/Form.js';
 
 function App() { // change to class
   return (
     <div className="App">
       <header className="App-header">
         <h1>Infinity</h1>
+        <Form />
       </header>
     </div>
   );

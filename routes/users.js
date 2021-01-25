b9e3d107de668eb86aaadd25f0304de94b1913a2
Khalilah <dygkhalilah@gmail.com>
2021-01-25 16:53:27 +0800

    Connected to API Localhost:5000

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+// const db = require("../model/helper");
+const bodyParser = require("body-parser");
+
+router.use(bodyParser.json());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {

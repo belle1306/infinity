ff9c5cb98ad30ca7e9c363d0558a31698aa4da64
Khalilah <dygkhalilah@gmail.com>
2021-01-26 14:08:41 +0800

    LogIn and SignUp page UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,40 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Form from './components/Form.js';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() { // change to class
-  return (
+import Login from "./components/LogIn.js";
+import SignUp from "./components/SignUp.js";
+
+function App() {
+  return (<Router>
     <div className="App">
-      <header className="App-header">
-        <h1>Infinity</h1>
-        <Form />
-      </header>
-    </div>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>RemoteStack</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Sign in</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="outer">
+        <div className="inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
   );
 }
 

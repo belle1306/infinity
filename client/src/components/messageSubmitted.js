2a6c209c791f12f537c08757233e4a00165704fc
Khalilah <dygkhalilah@gmail.com>
2021-02-04 00:23:43 +0800

    created my first animation with react-animated-css

--- client/src/components/messageSubmitted.js
+++ client/src/components/messageSubmitted.js
@@ -1,12 +1,15 @@
 // import React from "react";
 import {Link} from "react-router-dom";
 import React, { Component } from "react";
+import {Animated} from 'react-animated-css'
 
 class MessageSubmitted extends Component {
     render() {
         return (
         <div id="messageSubmitted">
-            <h2>⏁⊑⏃⋏☍ ⊬⍜⎍</h2>
+            <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
+                <h2>⏁⊑⏃⋏☍ ⊬⍜⎍</h2>
+            </Animated>
                 <h2>It means thank you in alien language!</h2>
             <br/>
             <p>Your message is on its way to the aliens!

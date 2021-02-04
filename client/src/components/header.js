2a0031f9c2f0258a3ef91cfd0afcdc99a0a1526c
Khalilah <dygkhalilah@gmail.com>
2021-02-04 16:12:47 +0800

    Retrieving from previous changes

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -8,21 +8,15 @@ export default function Header() {
         <div className='logo'>Infinity.</div>
         <nav>
           <ul>
-            <li className='discover'>
-<<<<<<< HEAD
-              <a href='/discover'>discover</a>
-            </li>
+            {/* <li className='Quiz'>
+              <a href='/Quiz'>Quiz</a>
+            </li> */}
             <li className='solarscope'>
               <a href='/solarscope'>Explore Solar System</a>
             </li>
             <li className='apod'>
               <a href='/apod'>Your astronomical birthday</a>
             </li>
-=======
-              <a href='/game'>discover</a>
-            </li>
-            
->>>>>>> 7e5cf2a4a6f868866c2300aa525cba7bcb9c6638
             <li>
               <a href='/message'>Send message</a>
               {/* <Link to="/message">Message</Link> */}

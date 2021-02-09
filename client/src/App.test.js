8968f92bf93c48b8269c3c1f70d91bb19aaf48a8
belle1306 <belle1306@gmail.com>
2021-02-09 15:50:50 +0800

    merged with main

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,26 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+// import React from "react";
+// import App from "./App";
+// // import Routes from "./routes";
+// // import MessageForm from "./components/messageForm";
+// // import APOD from "./components/apod";
+// // import Header from "./components/header";
+// // import MessageSubmitted from "./components/messageSubmitted";
+// // import ThreeHome from "./components/Three";
+// // import {jest} from '@jest/globals';
+// import {shallow} from "enzyme";
+// import MessageSubmitted from "./components/messageSubmitted";
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+// describe("rendering components", () => {
+//   // it("renders App component without crashing", () => {
+//   //   let wrapper = shallow(<App />);
+//   //   expect(wrapper.contains(<Routes />)).toEqual(true);
+//   // });
+//   test("renders MessageSubmitted component without crashing", () => {
+//     const wrapper = shallow(<MessageSubmitted />);
+//     const header =(<h2>It means 'thank you' in alien language!</h2>);
+//     expect(wrapper.contains(header)).toEqual(true);
+//   });
+//   // it("renders button", () => {
+//   //   const wrapper = mount(<MessageForm  )
+//   // })
+// })

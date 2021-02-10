db2dcfea90df6b41eed6616e4e8982737fda3d11
Khalilah <dygkhalilah@gmail.com>
2021-02-10 10:46:58 +0800

    Update styling for MessageText component

--- client/src/components/messageText.js
+++ client/src/components/messageText.js
@@ -17,18 +17,26 @@ class MessageText extends Component {
     
     getQuote() {
         let url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+        // let url ='https://type.fit/api/quotes'
 
         axios.get(url)
         .then(res => {
             let data = res.data.quotes
-            let quoteNum = Math.floor(Math.random() * data.length)
-            let randomQuote = data[quoteNum]
+            let quoteNum = Math.floor(Math.random() * data.length) //quote num
+            let randomQuote = data[quoteNum] // actual length
 
             this.setState({
                 quote: randomQuote["quote"],
                 author: randomQuote["author"]
             })
         })
+        // fetch("https://type.fit/api/quotes")
+        // .then(function(response) {
+        //     return response.json();
+        // })
+        // .then(function(data) {
+        //     console.log(data);
+        // });
     }
 
     getNewQuote = () => {
@@ -41,7 +49,7 @@ class MessageText extends Component {
         return (
             <div id='wrapper'>
                 <div id="quote-box">
-                    <QuoteBox quote={quote} author={author} />
+                    <QuoteBox quote={quote} author={author} /> 
                 </div>
                 <div>
                 <Button onClick={this.getNewQuote} />
@@ -62,10 +70,12 @@ class MessageText extends Component {
 // Quote Box component
 const QuoteBox = ({ quote, author }) => { //destructuring
         return (
-        <div>
-            <div id='text'><p>{quote}</p></div>
-            <div id='author'><p>"{author}"</p></div>
-        </div>
+            <React.Fragment>
+                <div>
+                    <div id='text'><p>{quote}</p></div>
+                    <div id='author'><p>"{author}"</p></div>
+                </div>
+            </React.Fragment>
         )
     }
 
